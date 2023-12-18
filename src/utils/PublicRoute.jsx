f76e18df24222ec35d126f5307f8d3b3cf263c19
Enam <mdenamahmedchowdhury@gmail.com>
2023-12-18 11:16:40 +0600

    error solved in pulic route

--- src/utils/PublicRoute.jsx
+++ src/utils/PublicRoute.jsx
@@ -3,8 +3,8 @@ import { Navigate } from "react-router-dom";
 
 const PublicRoute = ({ children }) => {
   //   const { user } = useAuth();
-  const user = "Enam";
-  return user?.email != null ? children : <Navigate to="/" />;
+  const user = null;
+  return user?.email == null ? children : <Navigate to="/" />;
 };
 
 export default PublicRoute;
